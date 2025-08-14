25afa36ce828ea2a7f7cc07889dc0ed449576b71
Kent <khieonqt@gmail.com>
2025-08-14 13:13:47 +0800

    Added String Formatting Class and methods

---
+++ LibrarySystem/StringFormat.java
@@ -0,0 +1,24 @@
+public class StringFormat {
+    public static String formatOutputString(String string) {
+        if (string.isEmpty()) {
+            return string;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String[] parts = string.split(" ");
+
+        for (String part : parts) {
+            sb.append(Character.toUpperCase(part.charAt(0))).append(part.substring(1)).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static String formatStoredString(String string) {
+        if (string.isEmpty()) {
+            return string;
+        }
+
+        return string.toLowerCase().trim();
+    }
+}
