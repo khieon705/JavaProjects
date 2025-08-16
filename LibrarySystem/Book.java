dc751f3441fde2ff9390068a13aa6ab69f5f5b97
Kent <khieonqt@gmail.com>
2025-08-16 13:32:37 +0800

    Remove InputSanitizer methods and change toString implementation

--- LibrarySystem/Book.java
+++ LibrarySystem/Book.java
@@ -1,11 +1,11 @@
 public record Book(String title, String author) {
     public Book(String title, String author) {
-        this.title = StringFormat.formatStoredString(title);
-        this.author = StringFormat.formatStoredString(author);
+        this.title = title;
+        this.author = author;
     }
 
     @Override
     public String toString() {
-        return "Book Title: " + StringFormat.formatOutputString(title) + ", Author: " + StringFormat.formatOutputString(author);
+        return "Book(title=" + title + ", author=" + author + ")";
     }
 }
