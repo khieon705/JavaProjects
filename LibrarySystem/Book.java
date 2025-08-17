04f2fe5ec8d1e45336082802c5a59f0a9bb41fd4
Kent <khieonqt@gmail.com>
2025-08-17 12:15:24 +0800

    Remove explicity book constructor

--- LibrarySystem/Book.java
+++ LibrarySystem/Book.java
@@ -1,9 +1,4 @@
 public record Book(String title, String author) {
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
-
     @Override
     public String toString() {
         return "Book(title=" + title + ", author=" + author + ")";
