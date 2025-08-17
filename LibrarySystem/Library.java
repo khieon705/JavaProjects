c7119cd663df98ca0f9c17def84b133a7a718de4
Kent <khieonqt@gmail.com>
2025-08-17 12:20:51 +0800

    Refactored borrowBook and returnBook, Added getAllLoanHistory and getLoanFee

--- LibrarySystem/Library.java
+++ LibrarySystem/Library.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Library {
     private final HashMap<Book, Integer> bookCollection;
@@ -21,63 +21,73 @@ public class Library {
         }
     }
 
-    public void addPatron(Patron patron) {
-        patronRegistry.addPatron(patron);
+    public boolean addPatron(Patron patron) {
+        return patronRegistry.addPatron(patron);
     }
 
-    public void borrowBook(Book book, String name) {
+    public BorrowResult borrowBook(Book book, String name) {
         if (!bookCollection.containsKey(book)) {
             System.out.println("Book is not available in this library");
-            return;
+            return BorrowResult.BOOK_NOT_FOUND;
         }
 
         Patron patron = patronRegistry.getPatron(name);
 
         if (patron == null) {
             System.out.println("Patron is not registered. Please register first");
-            return;
+            return BorrowResult.PATRON_NOT_REGISTERED;
         }
 
         if (bookCollection.get(book) == 0) {
             System.out.println("Book is not available");
-            return;
+            return BorrowResult.NO_COPIES_AVAILABLE;
         }
 
         reduceCopy(book);
         Loan loan = new Loan(patron, book, LocalDate.now().plusDays(7), LocalDate.now());
         loanRecord.addLoan(loan);
         patron.addLoan(loan);
+        return BorrowResult.SUCCESS;
     }
 
-    public void returnBook(Book book, String name) {
+    public ReturnResult returnBook(Book book, String name) {
         if (!bookCollection.containsKey(book)) {
-            System.out.println("Book is not from this library");
-            return;
+            return ReturnResult.NOT_LIBRARY_BOOK;
         }
 
         Patron patron = patronRegistry.getPatron(name);
+
+        if (patron == null) {
+            return ReturnResult.PATRON_NOT_REGISTERED;
+        }
+
         Loan loan = loanRecord.getLoan(patron, book);
 
         if (loan == null) {
-            return;
+            return ReturnResult.LOAN_NOT_FOUND;
         }
 
         loanRecord.updateLoanStatus(LocalDate.now(), loan);
-        loanRecord.viewLoan(loan);
         increaseCopy(book);
+        return loan.isOverDue(LocalDate.now());
     }
 
-    public void viewLoanHistory(String name) {
-        patronRegistry.viewPatronHistory(name);
-    }
+    public List<Loan> getLoanHistoryForPatron(String name) {
+        Patron patron = patronRegistry.getPatron(name);
 
-    public void viewLoanHistory() {
-        ArrayList<Loan> record = loanRecord.getLoanRecord();
-
-        System.out.println("=== Loan History ===");
-        for (Loan loan : record) {
-            System.out.println(loan);
+        if (patron == null) {
+            return null;
         }
+
+        return patron.getRecord();
+    }
+
+    public List<Loan> getAllLoanHistory() {
+        return loanRecord.getLoanRecord();
+    }
+
+    public long getLoanFee(Loan loan) {
+        return loanRecord.loanFee(loan);
     }
 
     private void reduceCopy(Book book) {
