9b3198aafc388bdfbb8157f9e8a4f0b946e824d5
Kent <khieonqt@gmail.com>
2025-08-17 13:13:11 +0800

    Refactored borrow, return, add book methods and replace bookcollection with an instance of BookCollection Class

--- LibrarySystem/Library.java
+++ LibrarySystem/Library.java
@@ -1,24 +1,19 @@
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
 
 public class Library {
-    private final HashMap<Book, Integer> bookCollection;
+    private final BookCollection bookCollection;
     private final PatronRegistry patronRegistry;
     private final LoanService loanRecord;
 
     public Library() {
-        bookCollection = new HashMap<>();
+        bookCollection = new BookCollection();
         patronRegistry = new PatronRegistry();
         loanRecord = new LoanService();
     }
 
     public void addBook(Book book, int numberOfCopies) {
-        if (bookCollection.containsKey(book)) {
-            bookCollection.replace(book, bookCollection.get(book) + numberOfCopies);
-        } else {
-            bookCollection.put(book, numberOfCopies);
-        }
+        bookCollection.addBook(book, numberOfCopies);
     }
 
     public boolean addPatron(Patron patron) {
@@ -26,50 +21,47 @@ public class Library {
     }
 
     public BorrowResult borrowBook(Book book, String name) {
-        if (!bookCollection.containsKey(book)) {
-            System.out.println("Book is not available in this library");
-            return BorrowResult.BOOK_NOT_FOUND;
-        }
+        if (bookCollection.hasBook(book)) {
+            Patron patron = patronRegistry.getPatron(name);
 
-        Patron patron = patronRegistry.getPatron(name);
+            if (patron == null) {
+                return BorrowResult.PATRON_NOT_REGISTERED;
+            }
 
-        if (patron == null) {
-            System.out.println("Patron is not registered. Please register first");
-            return BorrowResult.PATRON_NOT_REGISTERED;
-        }
+            if (bookCollection.isAvailable(book)) {
+                bookCollection.reduceCopy(book);
+                Loan loan = new Loan(patron, book, LocalDate.now().plusDays(7), LocalDate.now());
+                loanRecord.addLoan(loan);
+                patron.addLoan(loan);
+                return BorrowResult.SUCCESS;
+            }
 
-        if (bookCollection.get(book) == 0) {
-            System.out.println("Book is not available");
             return BorrowResult.NO_COPIES_AVAILABLE;
         }
 
-        reduceCopy(book);
-        Loan loan = new Loan(patron, book, LocalDate.now().plusDays(7), LocalDate.now());
-        loanRecord.addLoan(loan);
-        patron.addLoan(loan);
-        return BorrowResult.SUCCESS;
+        return BorrowResult.BOOK_NOT_FOUND;
     }
 
     public ReturnResult returnBook(Book book, String name) {
-        if (!bookCollection.containsKey(book)) {
-            return ReturnResult.NOT_LIBRARY_BOOK;
+        if (bookCollection.hasBook(book)) {
+            Patron patron = patronRegistry.getPatron(name);
+
+            if (patron == null) {
+                return ReturnResult.PATRON_NOT_REGISTERED;
+            }
+
+            Loan loan = loanRecord.getLoan(patron, book);
+
+            if (loan == null) {
+                return ReturnResult.LOAN_NOT_FOUND;
+            }
+
+            loanRecord.updateLoanStatus(LocalDate.now(), loan);
+            bookCollection.increaseCopy(book);
+            return loan.isOverDue(LocalDate.now());
         }
 
-        Patron patron = patronRegistry.getPatron(name);
-
-        if (patron == null) {
-            return ReturnResult.PATRON_NOT_REGISTERED;
-        }
-
-        Loan loan = loanRecord.getLoan(patron, book);
-
-        if (loan == null) {
-            return ReturnResult.LOAN_NOT_FOUND;
-        }
-
-        loanRecord.updateLoanStatus(LocalDate.now(), loan);
-        increaseCopy(book);
-        return loan.isOverDue(LocalDate.now());
+        return ReturnResult.NOT_LIBRARY_BOOK;
     }
 
     public List<Loan> getLoanHistoryForPatron(String name) {
@@ -88,13 +80,5 @@ public class Library {
 
     public long getLoanFee(Loan loan) {
         return loanRecord.loanFee(loan);
-    }
-
-    private void reduceCopy(Book book) {
-        bookCollection.replace(book, bookCollection.get(book) - 1);
-    }
-
-    private void increaseCopy(Book book) {
-        bookCollection.replace(book, bookCollection.get(book) + 1);
     }
 }
